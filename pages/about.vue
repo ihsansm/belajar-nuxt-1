cac29ed36be7a6b7856dd8a41bc65ce0b43b91a0
ihsan <ihsantwf301@gmail.com>
2025-01-06 21:23:23 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>
+        <h1>About</h1>
+    </div>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nuxt-link to="/about">About</nuxt-link>
+        <br>
+        <nuxt-link to="/notes">Notes</nuxt-link>
+        <br>
+        <nuxt-link :to="`/notes/${randomNumber()}`">Notes By Id</nuxt-link>
+        
+        <h1>Welcome</h1>
+    </div>
+</template>
+
+<script setup>
+const randomNumber = () => {
+Math.floor(Math.random() * 100) + 1;
+} 
+</script>
+
+<style></style>
