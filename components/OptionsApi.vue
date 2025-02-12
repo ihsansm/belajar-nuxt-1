07f9b59d89bdf76e30916877ef04d0bcece5f272
ihsan <ihsantwf301@gmail.com>
2025-02-12 20:54:06 +0700

    feat: finalize OptionsApi component with additional HTML tags and mounted hook

--- components/OptionsApi.vue
+++ components/OptionsApi.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <h4>Function : {{ hello() }}</h4>
     </div>
 </template>
 
--- components/compositionapi.vue
+++ components/compositionapi.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <h4>Function : {{ hello() }}</h4>
+
     </div>
 </template>
 
 <script setup>
 const title=ref('Composition Api')
 const hello=()=>{
-    console.log('Hello from Composition Api')
+    return 'Hello from Composition Api'
 }
+onMounted(()=> title.value='Hello Composition Api')
 </script>
 
 <style>
