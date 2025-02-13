98c0dece2cbf1a42914023fc9ec06af00500127e
sukiiiiiii <144516427+ihsansm@users.noreply.github.com>
2025-02-13 22:27:45 +0700

    Update banner.vue

--- components/banner.vue
+++ components/banner.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Welcome To Out Notes App!</h1>
+        <h1>Welcome To Out Notes Apps!</h1>
         <!-- <img v-bind:src="imgSrc" alt=""> -->
         <!-- <h1>{{ istitle }}</h1>
         <h1>{{ isfive==5?'Number is 5' : 'Number is not equal to 5' }}</h1> -->
